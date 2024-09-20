2d4d24ee2ef52fdc53d350ea3e69c5cd619d2249
2morrow <morrisaxel963@gmail.com>
2024-09-20 11:26:27 -0500

    Fin del Backend

--- server/src/config/db.ts
+++ server/src/config/db.ts
@@ -9,7 +9,7 @@ dotenv.config()
 // console.log(process.env.DATABASE_URL)
 
 const db = new Sequelize(process.env.DATABASE_URL!, {
-    models : [__dirname + '/../models/**/*.ts'],
+    models : [__dirname + '/../models/**/*'],
     logging: false,
 })
 
